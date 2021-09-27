777b767eff0a34112bdbde383bdc5a3cf31bacee
mityok <ptdimitry@gmail.com>
2021-09-27 23:29:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-qknhrv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-qknhrv)
---
+++ index.ts
@@ -0,0 +1,30 @@
+// Import stylesheets
+import './style.css';
+import * as Matter from 'matter-js';
+
+var Engine = Matter.Engine,
+  Render = Matter.Render,
+  World = Matter.World,
+  Bodies = Matter.Bodies;
+
+var engine = Engine.create();
+
+var render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    wireframes: true,
+  },
+});
+
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var ballA = Bodies.circle(380, 100, 40, 10);
+var ballB = Bodies.circle(460, 10, 40, 10);
+var ground = Bodies.rectangle(400, 380, 810, 60, { isStatic: true });
+
+World.add(engine.world, [boxA, ballA, ballB, ground]);
+
+Engine.run(engine);
+Render.run(render);
